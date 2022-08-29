fbca2f9937673db4a3ccda62c673c123b28115bd
Michail Petrov <Gismanus@gmail.com>
2022-08-29 16:46:59 +0300

    running Robot

--- 07_Robot.js
+++ 07_Robot.js
@@ -58,8 +58,42 @@ let first = new VillageState(
     "Post Office",
     [{ place: 'Post Office', address: 'Alice\'s House' }]
 );
-let next = first.move('Marketplace');
+let next = first.move('Alice\'s House');
 console.log(first);
 console.log(next);
-//console.log(next.parcels);
+
+function runRobot(state, robot, memory) {
+    for (let turn = 0; ; turn++) {
+        if (state.parcels.length == 0) {
+            consolele.log(`Completed in ${turn} moves`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action, direction);
+        memory = action.memory;
+        console.log(`Move in ${action.direction}`);
+    }
+}
+
+function randomPick(array){
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state){
+    return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while(place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState('Post Office', parcels);
+}
 
